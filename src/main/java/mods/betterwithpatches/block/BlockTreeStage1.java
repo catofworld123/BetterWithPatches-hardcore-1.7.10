7447bb24fd3b76367d42cb9196656e35042e3049
cat_of_world <Karacharov123@yandex.ru>
2024-07-10 15:42:01 +0500

    I did it + code optimisation2 + render

--- src/main/java/mods/betterwithpatches/block/BlockTreeStage1.java
+++ src/main/java/mods/betterwithpatches/block/BlockTreeStage1.java
@@ -1,35 +1,19 @@
 package mods.betterwithpatches.block;
 
-import betterwithmods.BWRegistry;
-import betterwithmods.blocks.BlockAesthetic;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import mods.betterwithpatches.BWPRegistry;
 import mods.betterwithpatches.block.tile.TileEntityBlockTreeStage1;
 import mods.betterwithpatches.proxy.ClientProxy;
-import mods.betterwithpatches.util.BWPConstants;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import java.util.List;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
-import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 
 public class BlockTreeStage1 extends BlockContainer {
-    public IIcon[] icons = new IIcon[6];
 
     public BlockTreeStage1() {
         super(Material.wood);
@@ -80,7 +64,7 @@ public class BlockTreeStage1 extends BlockContainer {
     @Override
     public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z) {
         {
-            this.setBlockBounds(0.0F, 0.0F, 0.125F, 1.0F, 1.0F, 0.875F);
+            this.setBlockBounds(0.0F, 0.0F, 0F, 1.0F, 1.0F, 1F);
         }
 
     }
--- src/main/java/mods/betterwithpatches/client/RenderBlockTreeStage1.java
+++ src/main/java/mods/betterwithpatches/client/RenderBlockTreeStage1.java
@@ -14,32 +14,22 @@ public class RenderBlockTreeStage1 implements ISimpleBlockRenderingHandler {
     public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
         renderer.setRenderBounds(0.125, 0D, 0.125, 0.875, 0.25, 0.875);
         renderItemBlock(block, renderer, metadata);
-        renderer.setRenderBounds(0.25, 0.25, 0.1875, 0.75, 0.3125, 0.8125);
+        renderer.setRenderBounds(0, 0, 0, 1, 0.0625, 1);
         renderItemBlock(block, renderer, metadata);
-        renderer.setRenderBounds(0.375, 0.3125, 0.25, 0.625, 0.625, 0.75);
+        renderer.setRenderBounds(0.0625, 0.0625, 0.0625, 0.9375, 0.9375, 0.9375);
         renderItemBlock(block, renderer, metadata);
-        renderer.setRenderBounds(0.1875, 0.625, 0, 0.8125, 1, 1);
+        renderer.setRenderBounds(0, 0.9375, 0, 1, 1, 1);
         renderItemBlock(block, renderer, metadata);
     }
 
     @Override
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
-        int meta = world.getBlockMetadata(x, y, z);
         renderer.setRenderBounds(0.125, 0D, 0.125, 0.875, 0.25, 0.875);
-        renderer.renderStandardBlock(block, x, y, z);
-        if (meta == 0) {
-            renderer.setRenderBounds(0.1875, 0.25, 0.25, 0.8125, 0.3125, 0.75);
+            renderer.setRenderBounds(0, 0, 0, 1, 0.0625, 1);
             renderer.renderStandardBlock(block, x, y, z);
-            renderer.setRenderBounds(0.25, 0.3125, 0.375, 0.75, 0.625, 0.625);
+            renderer.setRenderBounds(0.0625, 0.0625, 0.0625, 0.9375, 0.9375, 0.9375);
             renderer.renderStandardBlock(block, x, y, z);
-            renderer.setRenderBounds(0, 0.625, 0.1875, 1, 1, 0.8175);
-        } else {
-            renderer.setRenderBounds(0.25, 0.25, 0.1875, 0.75, 0.3125, 0.8125);
-            renderer.renderStandardBlock(block, x, y, z);
-            renderer.setRenderBounds(0.375, 0.3125, 0.25, 0.625, 0.625, 0.75);
-            renderer.renderStandardBlock(block, x, y, z);
-            renderer.setRenderBounds(0.1875, 0.625, 0, 0.8125, 1, 1);
-        }
+            renderer.setRenderBounds(0, 0.9375, 0, 1, 1, 1);
         renderer.renderStandardBlock(block, x, y, z);
         return true;
     }
